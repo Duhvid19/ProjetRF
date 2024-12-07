b1048ffcdf6a4c297480d8a65d209ea2051face0
David Lin <david.lin@etu.sorbonne-universite.fr>
2024-12-07 13:01:11 +0100

    changement du chemin pour faire les tests sur toutes les methodes

--- main.c
+++ main.c
@@ -4,30 +4,34 @@
 #include "test.h"
 
 int main(){
-    const char *data_directory = "./=SharvitB2/=Signatures/=ART";
-    Dataset *dataset = load_dataset(data_directory);
-    //normalize_dataset(dataset);
-    if (!dataset){
-        fprintf(stderr, "Erreur : Impossible de charger le jeu de donnees.\n");
-        return EXIT_FAILURE;
+    const char *data_directory[] = {"./=SharvitB2/=Signatures/=ART", "./=SharvitB2/=Signatures/=GFD", "./=SharvitB2/=Signatures/=Yang", "./=SharvitB2/=Signatures/=Zernike7"};
+
+    for (int i = 0; i < 4; i++){
+        Dataset *dataset = load_dataset(data_directory[i]);
+        //normalize_dataset(dataset);
+        if (!dataset){
+            fprintf(stderr, "Erreur : Impossible de charger le jeu de donnees.\n");
+            return EXIT_FAILURE;
+        }
+        const char *name = strrchr(data_directory[i], '=');
+        printf("\nMethode : %s\n", name + 1);
+
+        printf("=== KMEANS ===\n");
+        int k_clusters = 12;
+        int *clusters = malloc(dataset->data_count * sizeof(int));
+        kmeans(dataset, k_clusters, 100, clusters);
+        printf("Indice de silhouette : %f\n", silhouette_score(dataset, clusters, k_clusters));
+
+        printf("=== KNN ===\n");
+        int k_neighbors = 12;
+        int **confusion_matrix = init_conf_matrix();
+        float accuracy_knn = cross_validate(dataset, k_neighbors, dataset->data_count, confusion_matrix);
+        print_metrics(confusion_matrix);
+        printf("Accuracy : %.2f\n", accuracy_knn);
+
+        free(clusters);
+        free_dataset(dataset);
     }
 
-    printf("Jeu de donnees charge avec %d donnees.\n", dataset->data_count);
-
-    printf("\n=== KMEANS ===\n");
-    int k_clusters = 12;
-    int *clusters = malloc(dataset->data_count * sizeof(int));
-    kmeans(dataset, k_clusters, 100, clusters);
-    printf("Indice de silhouette : %f\n", silhouette_score(dataset, clusters, k_clusters));
-
-    printf("\n=== KNN ===\n");
-    int k_neighbors = 12;
-    int **confusion_matrix = init_conf_matrix();
-    float accuracy_knn = cross_validate(dataset, k_neighbors, dataset->data_count, confusion_matrix);
-    print_metrics(confusion_matrix);
-    printf("Accuracy : %.2f\n", accuracy_knn);
-
-    free(clusters);
-    free_dataset(dataset);
     return EXIT_SUCCESS;
 }
